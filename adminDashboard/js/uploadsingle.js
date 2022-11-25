444b769c8d36d82716de72401a363e89ca132b60
Ekrem Gunes <95303574+ekremgunes@users.noreply.github.com>
2022-11-25 22:11:00 +0300

    Update uploadsingle.js

--- adminDashboard/js/uploadsingle.js
+++ adminDashboard/js/uploadsingle.js
@@ -1,20 +1,20 @@
 var openFile = function (event) {
     
-    var input = event.target;
+    var input = event.target
 
 
-    var reader = new FileReader();
+    var reader = new FileReader()
     reader.onload = function () {
 
-        var dataURL = reader.result;
-        var output = document.getElementById('output');
-        output.src = dataURL;
-        var outputDiv = document.getElementById("output-div");
-        var outputImgDiv = document.getElementById("output-img-div");
+        var dataURL = reader.result
+        var output = document.getElementById('output')
+        output.src = dataURL
+        var outputDiv = document.getElementById("output-div")
+        var outputImgDiv = document.getElementById("output-img-div")
 
-        outputImgDiv.style.display = "none";
-        output.style.display = "inline";
+        outputImgDiv.style.display = "none"
+        output.style.display = "inline"
 
     };
-    reader.readAsDataURL(input.files[0]);
+    reader.readAsDataURL(input.files[0])
 };
